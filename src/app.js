374e4bfec61034e85bc0e9894a3e05133ccf58af
krokowski.dx <krokowski.dx@gmail.com>
2017-12-19 23:33:47 +0100

    App refactored to use promises and axios.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const yargs = require('yargs');
-const geocode = require('./geocode');
-const weather = require('./weather');
+const axios = require('axios');
+
 const argv = yargs
   .options({
     a: {
@@ -14,17 +14,29 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+const APIkey =  '5c1cab6751071efd72d4c9f8830c8649';
+let encodedAddress = encodeURIComponent(argv.address);
+let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+axios.get(geocodeUrl).then((response) => {
+  if (response.data.status === 'ZERO_RESULTS') {
+    throw new Error('Unable to find that address.');
+  }
+
+  let lat = response.data.results[0].geometry.location.lat;
+  let lng = response.data.results[0].geometry.location.lng;
+  let weatherUrl = `https://api.forecast.io/forecast/${APIkey}/${lat},${lng}`;
+
+  console.log(response.data.results[0].formatted_address);
+  return axios.get(weatherUrl);
+}).then((response) => {
+  let temperature = response.data.currently.temperature;
+  let apparentTemperature = response.data.currently.apparentTemperature;
+  console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
+}).catch((err) => {
+  if (err.code === 'ENOTFOUND') {
+    console.log('Unable to connet to API servers.');
   } else {
-    console.log(results.address);
-    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
-      if (errorMessage) {
-        console.log(errorMessage);
-      } else {
-        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}`);
-      }
-    });
+    console.log(err.message);
   }
 });
