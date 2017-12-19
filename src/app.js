3cd1ab67c5b04b030f02ec2b3d31f02f4bf9b213
krokowski.dx <krokowski.dx@gmail.com>
2017-12-19 23:51:24 +0100

    Swapped lets for consts and farenheit to celsius.

--- src/app.js
+++ src/app.js
@@ -15,28 +15,35 @@ const argv = yargs
   .argv;
 
 const APIkey =  '5c1cab6751071efd72d4c9f8830c8649';
-let encodedAddress = encodeURIComponent(argv.address);
-let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+const encodedAddress = encodeURIComponent(argv.address);
+const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
-axios.get(geocodeUrl).then((response) => {
-  if (response.data.status === 'ZERO_RESULTS') {
-    throw new Error('Unable to find that address.');
-  }
+const toCelsius = tempF => Math.round(5/9*(tempF-32));
 
-  let lat = response.data.results[0].geometry.location.lat;
-  let lng = response.data.results[0].geometry.location.lng;
-  let weatherUrl = `https://api.forecast.io/forecast/${APIkey}/${lat},${lng}`;
+axios.get(geocodeUrl)
+  .then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+      throw new Error('Unable to find that address.');
+    }
 
-  console.log(response.data.results[0].formatted_address);
-  return axios.get(weatherUrl);
+    const lat = response.data.results[0].geometry.location.lat;
+    const lng = response.data.results[0].geometry.location.lng;
+    const weatherUrl = `https://api.forecast.io/forecast/${APIkey}/${lat},${lng}`;
+
+    console.log(response.data.results[0].formatted_address);
+    return axios.get(weatherUrl);
+
 }).then((response) => {
-  let temperature = response.data.currently.temperature;
-  let apparentTemperature = response.data.currently.apparentTemperature;
-  console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
+
+    const temperature = response.data.currently.temperature;
+    const apparentTemperature = response.data.currently.apparentTemperature;
+
+    console.log(`It's currently ${toCelsius(temperature)}\u00B0C. It feels like ${toCelsius(apparentTemperature)}\u00B0C.`);
+
 }).catch((err) => {
-  if (err.code === 'ENOTFOUND') {
-    console.log('Unable to connet to API servers.');
-  } else {
-    console.log(err.message);
-  }
+    if (err.code === 'ENOTFOUND') {
+      console.error('Unable to connet to API servers.');
+    } else {
+      console.error(err.message);
+    }
 });
